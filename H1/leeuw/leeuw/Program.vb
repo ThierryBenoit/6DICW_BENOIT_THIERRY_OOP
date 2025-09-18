1008fa49adcb2b2806c5242798530ba9305d10e4
ThierryBenoit <benoit.thierry2.00@gmail.com>
2025-09-18 12:28:07 +0200

    Hier een leeuw

---
+++ H1/leeuw/leeuw/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Class leeuw
+        Public intsnelheid As Integer
+        Public intleeftheid As Integer
+    End Class
+    Sub main(args As String())
+        Console.WriteLine("Ik ben een leeuw")
+        Dim objleeuw As New leeuw()
+    End Sub
+End Module
