9dc868dcbfe3f60c582e069e1296c3bb31a8e9c3
ThierryBenoit <benoit.thierry2.00@gmail.com>
2025-09-18 12:45:36 +0200

    Mijn rijbewijs

---
+++ H1/Rijbewijs/ConsoleApp1/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Class Rijbewijs
+        Public BlnHeeftrijbewijs As Boolean
+    End Class
+    Sub Main(args As String())
+        Console.WriteLine("True")
+    End Sub
+End Module
